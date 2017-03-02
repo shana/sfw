c2f3b4c4727d3c226f794cd66d0f07a794dc15a8
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-02 07:56:38 -0800

    Dll export sample

--- sfw/sfw.cpp
+++ sfw/sfw.cpp
@@ -1,11 +1,22 @@
-// sfw.cpp : Defines the entry point for the console application.
+// Win32Project1.cpp : Defines the exported functions for the DLL application.
 //
 
 #include "stdafx.h"
+#include "includes/sfw.h"
 
 
-int main()
+// This is an example of an exported variable
+WIN32PROJECT1_API int nWin32Project1 = 0;
+
+// This is an example of an exported function.
+WIN32PROJECT1_API int fnWin32Project1(void)
 {
-    return 0;
+	return 42;
 }
 
+// This is the constructor of a class that has been exported.
+// see Win32Project1.h for the class definition
+CWin32Project1::CWin32Project1()
+{
+	return;
+}
