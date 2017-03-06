378e9a1b7a4703e36110383a08d62d0011fd8254
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-06 08:23:47 -0500

    Forgot some changes for a compile

--- test-console/test-console.cpp
+++ test-console/test-console.cpp
@@ -14,39 +14,39 @@ int main()
 	auto continueFlag = true;
 	while (continueFlag)
 	{
-		auto fileEvents = NativeInterface_getEvents(native_interface);
-		if (fileEvents == nullptr || fileEvents->size() <= 0)
-		{
-			std::cout << "No events." << std::endl;
-		}
-		else
-		{
-			for (auto fileEvent = fileEvents->begin(); fileEvent != fileEvents->end(); ++fileEvent)
-			{
-				auto evt = *fileEvent;
-				std::cout << "Change: ";
-				switch (evt->type)
-				{
-				case DELETED:
-					std::cout << "Deleted: ";
-					break;
-
-				case CREATED:
-					std::cout << "Created: ";
-					break;
-
-				case MODIFIED:
-					std::cout << "Modified: ";
-					break;
-
-				case RENAMED:
-					std::cout << "Renamed: ";
-					break;
-				}
-
-				std::cout << evt->fileA << std::endl;
-			}
-		}
+//		auto fileEvents = NativeInterface_getEvents(native_interface);
+//		if (fileEvents == nullptr || fileEvents->size() <= 0)
+//		{
+//			std::cout << "No events." << std::endl;
+//		}
+//		else
+//		{
+//			for (auto fileEvent = fileEvents->begin(); fileEvent != fileEvents->end(); ++fileEvent)
+//			{
+//				auto evt = *fileEvent;
+//				std::cout << "Change: ";
+//				switch (evt->type)
+//				{
+//				case DELETED:
+//					std::cout << "Deleted: ";
+//					break;
+//
+//				case CREATED:
+//					std::cout << "Created: ";
+//					break;
+//
+//				case MODIFIED:
+//					std::cout << "Modified: ";
+//					break;
+//
+//				case RENAMED:
+//					std::cout << "Renamed: ";
+//					break;
+//				}
+//
+//				std::cout << evt->fileA << std::endl;
+//			}
+//		}
 
 		std::cout << "Press any key or 'q' to quit: ";
 		auto inputChar = std::cin.get();
