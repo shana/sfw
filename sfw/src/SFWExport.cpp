676e6841370d3f1ab61c755393b7cc27fbf6d28b
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-08 08:30:53 -0500

    Replacing _strdup

--- sfw/src/SFWExport.cpp
+++ sfw/src/SFWExport.cpp
@@ -18,6 +18,30 @@ std::string NativeInterface_getError(NativeInterface* instance)
 	return native_interface->getError();
 }
 
+char* copyString(std::string orig)
+{
+	if(orig.length() == 0)
+	{
+		return nullptr;
+	}
+
+	auto orig_cstr = orig.c_str();
+
+	if (!orig_cstr)
+	{
+		return nullptr;
+	}
+
+	int len = strlen(orig_cstr) + 1;
+
+	auto buf = static_cast<char*>(malloc(len));
+	if (buf) {
+		memcpy(buf, orig_cstr, len);
+	}
+
+	return buf;
+}
+
 void __stdcall NativeInterface_getEvents(NativeInterface* instance, SFW_Event** events, int* eventsCount)
 {
 	auto eventVector = instance->getEvents();
@@ -27,27 +51,18 @@ void __stdcall NativeInterface_getEvents(NativeInterface* instance, SFW_Event** 
 	{
 		*events = new SFW_Event[eventVector->size()];
 
-		for (auto &localEvent : *eventVector)
+		for (auto& localEvent : *eventVector)
 		{
 			auto exportEvent = *events + count;
 
 			exportEvent->type = localEvent->type;
 
-			exportEvent->directory = _strdup(localEvent->directory.c_str());
-			exportEvent->fileA = _strdup(localEvent->fileA.c_str());
-			
-			if(localEvent->fileB.length() == 0)
-			{
-				exportEvent->fileB = nullptr;
-			}
-			else
-			{
-				exportEvent->fileB = _strdup(localEvent->fileB.c_str());
-			}
-			
+			exportEvent->directory = copyString(localEvent->directory);
+			exportEvent->fileA = copyString(localEvent->fileA);
+			exportEvent->fileB = copyString(localEvent->fileB);
+
 			count++;
 		}
-
 	}
 
 	*eventsCount = count;
