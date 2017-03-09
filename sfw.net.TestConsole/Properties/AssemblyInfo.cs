e275963dbc4c12845b40a0377406ba47fc18cd8d
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-09 10:03:42 -0500

    Renaming namespace

--- sfw.net.TestConsole/Properties/AssemblyInfo.cs
+++ sfw.net.TestConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("swf.net.TestConsole")]
+[assembly: AssemblyTitle("sfw.net.TestConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("swf.net.TestConsole")]
+[assembly: AssemblyProduct("sfw.net.TestConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
