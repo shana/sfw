20b4299925cb7e66c17aa9b3578bf3f6794d08ba
Andreia Gaita <shana@spoiledcat.net>
2017-08-31 22:04:13 +0200

    DLLImport for Unix needs to be libsfw

--- src/bindings/sfw.net/NativeInterfaceStatic.cs
+++ src/bindings/sfw.net/NativeInterfaceStatic.cs
@@ -175,19 +175,19 @@ namespace sfw.net
 
         static class NativeCalls
         {
-            [DllImport("sfw", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libsfw", CallingConvention = CallingConvention.Cdecl)]
             public static extern IntPtr NativeInterface_Create(string path);
 
-            [DllImport("sfw", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libsfw", CallingConvention = CallingConvention.Cdecl)]
             public static extern void NativeInterface_Delete(IntPtr nativeInterface);
 
-            [DllImport("sfw", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libsfw", CallingConvention = CallingConvention.Cdecl)]
             public static extern string NativeInterface_GetError(IntPtr nativeInterface);
 
-            [DllImport("sfw", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libsfw", CallingConvention = CallingConvention.Cdecl)]
             public static extern bool NativeInterface_HasErrored(IntPtr nativeInterface);
 
-            [DllImport("sfw", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libsfw", CallingConvention = CallingConvention.Cdecl)]
             public static extern void NativeInterface_GetEvents(IntPtr nativeInterface, out IntPtr events, out int bufferSize);
         }
     }
